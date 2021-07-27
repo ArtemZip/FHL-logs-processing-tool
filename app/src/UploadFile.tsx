e0cb87ab9dc2fb5c5750814879de7b63465f2855
Marta Maksymyak <mmaksymyak@microsoft.com>
2021-07-27 15:34:29 +0200

    added upload file component

---
+++ app/src/UploadFile.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, Component, CSSProperties } from "react";
+
+const inputUploadFile: CSSProperties = {
+  display: 'none',
+};
+
+const buttonUploadFile: CSSProperties = {
+    margin: 8,
+};
+
+interface UploadFileOwnProps { }
+interface UploadFileProps extends UploadFileOwnProps { }
+interface UploadFileStateProps { }
+
+export class UploadFileComponent extends Component<UploadFileProps, UploadFileStateProps>  {
+    private getFileFromInput(file: File): Promise<any> {
+        return new Promise(function (resolve, reject) {
+            const reader = new FileReader();
+            reader.onerror = reject;
+            reader.onload = function () { resolve(reader.result); };
+            reader.readAsBinaryString(file);
+        });
+    }
+
+    private manageUploadedFile(binary: String, file: File) {
+        // todo: display upload result and file info if necessary
+        console.log(`File size: ${binary.length}`);
+        console.log(`File name: ${file.name}`);
+    }
+
+    private handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+        event.persist();
+        const files: FileList | null = event.target.files;
+        if (files != null) {
+            Array.from(files).forEach(file => {
+                this.getFileFromInput(file)
+                    .then((binary) => {
+                        this.manageUploadedFile(binary, file);
+                    }).catch(function (reason) {
+                        console.log(`Error during upload ${reason}`);
+                        event.target.value = ''; // to allow upload of same file if error occurs
+                    });
+            });
+        }
+    }
+
+
+    public render(): JSX.Element {
+        return (
+            // todo: change style
+            <div className="App">
+                <header className="App-header">
+                    <input accept=".txt" style={inputUploadFile} id="file" multiple={true} type="file"
+                        onChange={this.handleFileChange.bind(this)} />
+                    <label htmlFor="file">
+                        Upload files
+                    </label>
+                </header>
+            </div>
+        );
+    }
+}
