ca038f8c1d6f7965b4e391dce840245e40d5ed9e
Artem Bosniak <artembosniak@microsoft.com>
2021-07-30 17:37:22 +0200

    tmp log parser

--- app/src/services/sources/tmpLogsParser.ts
+++ app/src/services/sources/tmpLogsParser.ts
@@ -1,7 +1,38 @@
-import type { ILogsParser, ParsingResult } from '../../types/logsParserInterfaces';
+import type { ILogsParser, ParsedLine, ParsingResult } from '../../types/logsParserInterfaces';
 
 export class TmpLogsParser implements ILogsParser {
+    private currentTimestamp: number | undefined;
+
     public parse(lines: string[]): ParsingResult {
-        throw new Error('Method not implemented.');
+        let parsedLines: ParsedLine[] = [];
+        lines.forEach(line => {
+            let l = this.parseLine(line);
+            if(l) {
+                parsedLines.push(l);
+            }
+        })
+        return {
+            parsedContent: parsedLines
+        };
+    }
+
+    private parseLine(line: string) : ParsedLine | undefined {
+        const dateTime = line.match(/^.*Z/)?.[0];
+        if (dateTime) {
+            const logLevel = line.match(/\b(?:Inf|Err|War)\b/)?.[0] ?? '';
+            const resource = line.match(`(?<=${logLevel}\s*).*: `)?.[0] ?? '';
+            const message = line.match(`(?<=${resource}\s*).*`)?.[0] ?? '';
+
+            this.currentTimestamp = Date.parse(dateTime);
+            const parsedLine = [`${Date.parse(dateTime)}`, `level='${logLevel.trim()}' resource='${resource.trim()}' msg='${message.trim()}'`];
+
+            return { columns: parsedLine };
+        }
+
+        if(this.currentTimestamp) {
+            return { columns: [`${this.currentTimestamp}`, `msg='${line}'`]}
+        }
+        return undefined;
+        
     }
 }
